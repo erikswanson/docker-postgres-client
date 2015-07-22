2de35915876d90e0f8b9eb402f3d636485a8eed2
Erik Swanson <erikswanson@users.noreply.github.com>
2015-07-22 15:16:51 -0700

    Update docker-entrypoint.sh
    Use POSTGRES_DB as the default PGDATABASE.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/bash -e
+#!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 export PGHOST=${POSTGRES_PORT_5432_TCP_ADDR?'Missing: --link <my-postgres>:postgres'}
 export PGPORT=${POSTGRES_PORT_5432_TCP_PORT?'Missing: --link <my-postgres>:postgres'}
 export PGUSER=${POSTGRES_ENV_POSTGRES_USER:-postgres}
-export PGDATABASE=$PGUSER
+export PGDATABASE=${POSTGRES_ENV_POSTGRES_DB:-${PGUSER}}
 
 if test -n "$POSTGRES_ENV_POSTGRES_PASSWORD"; then
 cat > ~/.pgpass <<EOF
